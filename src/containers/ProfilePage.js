5aaee1b15c7f35d1f5b78dfd433f54d1f2604dd1
Divineechidume <divineechidume@berkeley.edu>
2020-07-09 19:17:11 -0700

    basic questions filled in

---
+++ src/containers/ProfilePage.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import "./ProfilePage.css";
+import UserProfileForm from "../components/UserProfileForm";
+
+class ProfilePage extends Component {
+  state = {
+    FirstName: "",
+    LastName: "",
+    DateOfBirth: "",
+    LifeStyle: "",
+    RoomateOrHousemate: "",
+    Bio: "",
+    CurrentCity: "",
+    NewCity: "",
+    Budget: "",
+  };
+  render() {
+    return (
+      <div className="ProfilePage">
+        <div style={{ position: "absolute", left: 1500, top: 55 }}>
+          <p>
+            Hi <span style={{ fontWeight: "bold" }}>Karen</span>
+          </p>
+        </div>
+        <h2 className="headerProp">User Profile</h2>
+        <div className="profileContainer">
+          <div className="profileInnner">
+            <UserProfileForm />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProfilePage;
