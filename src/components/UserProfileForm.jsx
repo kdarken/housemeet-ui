2bc9d3c045bf0709bff3142b899908308c7b5806
Divineechidume <divineechidume@berkeley.edu>
2020-07-15 16:35:38 -0700

    currently debugging why profile isn't beign saved to mongodb

--- src/components/UserProfileForm.jsx
+++ src/components/UserProfileForm.jsx
@@ -1,21 +1,90 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "typeface-montserrat";
 
 class UserProfileForm extends Component {
-  state = {
-    FirstName: "",
-    LastName: "",
-    DateOfBirth: "",
-    LifeStyle: "",
-    RoomateOrHousemate: "",
-    Bio: "",
-    CurrentCity: "",
-    NewCity: "",
-    Budget: "",
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      firstName: "",
+      lastName: "",
+      dateOfBirth: "",
+      lifeStyle: "",
+      roommateOrHousemate: "",
+      bio: "",
+      currentCity: "",
+      newCity: "",
+      budget: "",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const value = event.target.value;
+    this.setState({
+      ...this.state,
+      [event.target.name]: value,
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    axios
+      .post("/profiles/update/basic", {
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        dateOfBirth: this.state.dateOfBirth,
+        lifeStyle: this.state.lifeStyle,
+        roomateOrHousemate: this.state.roomateOrHousemate,
+        bio: this.state.bio,
+        currentCity: this.state.currentCity,
+        newCity: this.state.newCity,
+        budget: this.state.budget,
+        email: "divineechidume@berkeley.edu",
+      })
+      .then(
+        (response) => {
+          console.log(response);
+          alert("Creating your profile...");
+        },
+        (error) => {
+          console.log(error);
+          alert("Invalid credentials.");
+        }
+      );
+  }
+
+  renderInput(name, stateName, groupClass, colSize1, colSize2, value) {
+    var size1 = colSize1 + " col-form-label";
+    var groupClass = "form-group " + groupClass;
+    return (
+      <div class={groupClass}>
+        <div class="form-group row">
+          <label for="inputEmail3" class={size1}>
+            {name}
+          </label>
+          <div class={colSize2}>
+            <input
+              type="text"
+              name={stateName}
+              id={stateName}
+              className="form-control"
+              placeholder={name}
+              value={value}
+              onChange={this.handleChange}
+              required
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   render() {
     return (
@@ -23,48 +92,86 @@ class UserProfileForm extends Component {
         <div class="row">
           <div class="col-sm-3">One of three columns</div>
           <div class="col-sm-9">
-            <form>
+            <form onSubmit={this.handleSubmit}>
               <div class="form-row">
-                {RenderInput(
+                {this.renderInput(
                   "First Name",
+                  "firstName",
                   "col-md-4",
                   "col-sm-5.5",
-                  "col-sm-7"
+                  "col-sm-7",
+                  this.state.firstName
                 )}
-                {RenderInput("Last Name", "col-md-4", "col-sm-5.5", "col-sm-7")}
-                {RenderInput(
-                  "Date of Birth",
+                {this.renderInput(
+                  "Last Name",
+                  "lastName",
                   "col-md-4",
                   "col-sm-5.5",
-                  "col-sm-7"
+                  "col-sm-7",
+                  this.state.lastName
+                )}
+                {this.renderInput(
+                  "Date of Birth",
+                  "dateOfBirth",
+                  "col-md-4",
+                  "col-sm-5.5",
+                  "col-sm-7",
+                  this.state.dateOfBirth
                 )}
               </div>
               <div class="form-row">
-                {RenderInput("Lifestyle", "col-md-4", "col-sm-5.5", "col-8")}
-                {RenderInput(
+                {this.renderInput(
+                  "Lifestyle",
+                  "lifeStyle",
+                  "col-md-4",
+                  "col-sm-5.5",
+                  "col-8",
+                  this.state.lifeStyle
+                )}
+                {this.renderInput(
                   "Looking for a Roommate of Housemate",
+                  "roommateOrHousemate",
                   "col-md-8",
                   "col-sm-5.5",
-                  "col"
+                  "col",
+                  this.state.roommateOrHousemate
                 )}
               </div>
               <div class="form-row">
-                {RenderInput(
+                {this.renderInput(
                   "Tell us About yourself",
+                  "bio",
                   "col-md-12",
                   "col-sm-5.5",
-                  "col"
+                  "col",
+                  this.state.bio
                 )}
               </div>
               <div class="form-row">
-                {RenderInput(
+                {this.renderInput(
                   "Current City",
+                  "currentCity",
                   "col-md-4",
                   "col-sm-5.5",
-                  "col-sm-7"
+                  "col-sm-7",
+                  this.state.currentCity
                 )}
-                {RenderInput("New City", "col-md-4", "col-sm-5.5", "col-sm-7")}
-                {RenderInput("Budget", "col-md-4", "col-sm-5.5", "col-sm-7")}
+                {this.renderInput(
+                  "New City",
+                  "newCity",
+                  "col-md-4",
+                  "col-sm-5.5",
+                  "col-sm-7",
+                  this.state.newCity
+                )}
+                {this.renderInput(
+                  "budget",
+                  "budget",
+                  "col-md-4",
+                  "col-sm-5.5",
+                  "col-sm-7",
+                  this.state.budget
+                )}
               </div>
               <button type="submit" class="btn btn-primary">
                 Submit Profile
@@ -75,28 +182,6 @@ class UserProfileForm extends Component {
       </div>
     );
   }
-}
-
-function RenderInput(name, groupClass, colSize1, colSize2) {
-  var size1 = colSize1 + " col-form-label";
-  var groupClass = "form-group " + groupClass;
-  return (
-    <div class={groupClass}>
-      <div class="form-group row">
-        <label for="inputEmail3" class={size1}>
-          {name}
-        </label>
-        <div class={colSize2}>
-          <input
-            type="text"
-            class="form-control"
-            id="inputEmail3"
-            placeholder={name}
-          />
-        </div>
-      </div>
-    </div>
-  );
 }
 
 ReactDOM.render(<UserProfileForm />, document.getElementById("root"));
